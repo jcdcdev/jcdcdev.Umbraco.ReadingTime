73fdd3a8830253565162f2cf035e7938c7fe0cf6
jcdcdev <james@jcdc.dev>
2025-04-22 00:49:20 +0100

    fix: PackageManifest version

--- src/jcdcdev.Umbraco.ReadingTime/Core/Composing/PackageManifestReader.cs
+++ src/jcdcdev.Umbraco.ReadingTime/Core/Composing/PackageManifestReader.cs
@@ -1,4 +1,5 @@
-﻿using jcdcdev.Umbraco.Core.Extensions;
+﻿using System.Reflection;
+using jcdcdev.Umbraco.Core.Extensions;
 using jcdcdev.Umbraco.Core.Web.Models.Manifests;
 using Umbraco.Cms.Core.Manifest;
 using Umbraco.Cms.Infrastructure.Manifest;
@@ -13,7 +14,7 @@ public class PackageManifestReader : IPackageManifestReader
         var packageManifest = new PackageManifest
         {
             Name = Constants.Package.Name,
-            Version = EnvironmentExtensions.CurrentAssemblyVersion().ToSemVer()?.ToString() ?? "0.1.0",
+            Version = Assembly.GetAssembly(typeof(PackageManifestReader))?.GetName().Version?.ToSemVer()?.ToString() ?? "0.1.0",
             AllowPublicAccess = false,
             AllowTelemetry = true,
             Extensions = []
